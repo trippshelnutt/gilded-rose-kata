5a25d85283d447c972f1ea17f0765518fae4030f
Tripp Shelnutt <trippshelnutt@outlook.com>
2020-06-12 23:43:12 -0400

    Original solution

---
+++ GildedRoseTests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using GildedRose;
+
+namespace GildedRoseTests
+{
+    public class ProgramTests 
+    {
+        [Test]
+        public void foo()
+        {
+            var items = new List<Item>{
+                new Item
+                {
+                    Name = "foo",
+                    SellIn = 0,
+                    Quality = 0
+                }
+            };
+            var app = new Program { Items = items };
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("foo", app.Items[0].Name);
+        }
+    }
+}
---
+++ GildedRoseTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GildedRoseTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GildedRoseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4951f40e-9407-4791-8e6f-5ea74ceff12f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
